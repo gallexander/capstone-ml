f5c3a033b78ce06893f506213c4d48625fb1b818
gallex <alexander.gallauner@gmail.com>
2023-03-12 19:23:55 +0100

    Add classifier to predict sex

--- dating_skeleton.py
+++ dating_skeleton.py
@@ -3,6 +3,9 @@ import pandas as pd
 import numpy as np
 from sklearn import preprocessing
 from matplotlib import pyplot as plt
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
 #Create your df here:
 df = pd.read_csv("profiles.csv")
 
@@ -29,11 +32,36 @@ x_scaled = min_max_scaler.fit_transform(x)
 
 feature_data = pd.DataFrame(x_scaled, columns=feature_data.columns)
 print(feature_data.columns)
+income_mean = df["income"].mean()
+df["income"] = df["income"].apply(lambda x: income_mean if x == -1 else x)
+print(set(df["education"].values))
 
-plt.hist(df.age, bins=20)
-plt.xlabel("Age")
-plt.ylabel("Frequency")
-plt.xlim(16, 80)
+sex_mapping = {"m": 0, "f": 1}
+df["sex"] = df["sex"].map(sex_mapping)
+education_mapping = {'working on med school':5, 'dropped out of med school':5, 'graduated from college/university':4, 'graduated from masters program':6, 'dropped out of law school':5, 'college/university':3, 'working on high school':1, 'masters program':5, 'dropped out of two-year college':2, 'working on college/university':3, 'two-year college':3, 'high school': 1, 'dropped out of space camp':6, 'law school':5, 'med school':5, 'dropped out of college/university':2, 'working on masters program':5, 'graduated from med school':6, 'dropped out of masters program':5, 'working on law school':4, 'graduated from space camp':7, 'graduated from ph.d program':7, 'graduated from law school':6, 'working on space camp':6, 'graduated from high school':1, 'working on two-year college':2, 'dropped out of high school':0, 'graduated from two-year college':4, 'ph.d program':6, 'working on ph.d program':6, 'dropped out of ph.d program':6, 'space camp':6, np.nan:3}
+df["education"] = df["education"].map(education_mapping)
+print(df["education"].value_counts())
+
+#plt.hist(df.age, bins=20)
+#plt.xlabel("Age")
+#plt.ylabel("Frequency")
+#plt.xlim(16, 80)
+
+first_question_data = df[["education", "income"]]
+x = first_question_data.values
+x_scaled = min_max_scaler.fit_transform(x)
+first_question_data = pd.DataFrame(x_scaled, columns=first_question_data.columns)
+X_train, X_test, y_train, y_test = train_test_split(first_question_data, df["sex"], train_size=0.8, test_size=0.2, random_state=42)
+
+scores = []
+for k in range(1,50):
+    classifier = KNeighborsClassifier(n_neighbors=k)
+    classifier.fit(X_train, y_train)
+    scores.append(classifier.score(X_test, y_test))
+
+plt.plot(range(1,50), scores, "-o")
+plt.xlabel("k")
+plt.ylabel("Score")
 plt.show()
 
 # %%
