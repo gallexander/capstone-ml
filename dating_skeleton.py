a0fb3748540f12dab34a9052b9aac49e93c4ded2
gallex <alexander.gallauner@gmail.com>
2023-03-15 07:53:34 +0100

    Filter nans and invalid values

--- dating_skeleton.py
+++ dating_skeleton.py
@@ -40,12 +40,14 @@ x_scaled = min_max_scaler.fit_transform(x)
 feature_data = pd.DataFrame(x_scaled, columns=feature_data.columns)
 print(feature_data.columns)
 income_mean = df["income"].mean()
-df["income"] = df["income"].apply(lambda x: income_mean if x == -1 else x)
+#df["income"] = df["income"].apply(lambda x: income_mean if x == -1 else x)
+df.drop(df[df["income"] == -1].index, axis=0, inplace=True)
 print(set(df["education"].values))
 
 sex_mapping = {"m": 1, "f": 0}
 df["sex"] = df["sex"].map(sex_mapping)
 education_mapping = {'working on med school':5, 'dropped out of med school':5, 'graduated from college/university':4, 'graduated from masters program':6, 'dropped out of law school':5, 'college/university':3, 'working on high school':1, 'masters program':5, 'dropped out of two-year college':2, 'working on college/university':3, 'two-year college':3, 'high school': 1, 'dropped out of space camp':6, 'law school':5, 'med school':5, 'dropped out of college/university':2, 'working on masters program':5, 'graduated from med school':6, 'dropped out of masters program':5, 'working on law school':4, 'graduated from space camp':7, 'graduated from ph.d program':7, 'graduated from law school':6, 'working on space camp':6, 'graduated from high school':1, 'working on two-year college':2, 'dropped out of high school':0, 'graduated from two-year college':4, 'ph.d program':6, 'working on ph.d program':6, 'dropped out of ph.d program':6, 'space camp':6, np.nan:3}
+df.dropna(subset=['education'], axis=0, inplace=True)
 df["education"] = df["education"].map(education_mapping)
 print(df["education"].value_counts())
 
@@ -55,28 +57,30 @@ print(df["education"].value_counts())
 #plt.xlim(16, 80)
 
 first_question_data = df[["education", "income"]]
-x = first_question_data.values
-x_scaled = min_max_scaler.fit_transform(x)
-first_question_data = pd.DataFrame(x_scaled, columns=first_question_data.columns)
-X_train, X_test, y_train, y_test = train_test_split(first_question_data, df["sex"], train_size=0.8, test_size=0.2)
+scaler = preprocessing.StandardScaler()
+df[["education", "income"]] = scaler.fit_transform(df[["education", "income"]])
+#x = first_question_data.values
+#x_scaled = min_max_scaler.fit_transform(x)
+#first_question_data = pd.DataFrame(x_scaled, columns=first_question_data.columns)
+X_train, X_test, y_train, y_test = train_test_split(first_question_data, df["sex"], test_size=0.25, random_state=98)
 
 #scores = []
 #for k in range(1,30):
 classifier = KNeighborsClassifier(n_neighbors=5)
 classifier.fit(X_train, y_train)
 y_pred = classifier.predict(X_test)
+print(classifier.score(X_test, y_test))
 print(confusion_matrix(y_true=y_test, y_pred=y_pred))
     
 classifier = LogisticRegression()
 classifier.fit(X_train, y_train)
 y_pred = classifier.predict(X_test)
+print(classifier.predict_proba(X_test)[:1])
 array = confusion_matrix(y_true=y_test, y_pred=y_pred)
-print(array[0][0])
 print(array)
+print(classifier.coef_)
+print(classifier.intercept_)
 print(sum(y_test))
-print(sum(y_pred))
-print(len(y_pred))
-print(y_pred)
 
 #plt.plot(range(1,30), scores, "-o")
 #plt.xlabel("k")
